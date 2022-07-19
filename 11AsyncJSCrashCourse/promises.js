96828add80b31450676e38c3d41aad70628a1f3f
Daniel Eastburn <danieleastburnWork@gmail.com>
2022-07-19 14:14:57 +0100

    Async JS Complete

--- 11AsyncJSCrashCourse/promises.js
+++ 11AsyncJSCrashCourse/promises.js
@@ -38,10 +38,33 @@ function createPost(post){
 
 
 
-// Promise.all
-const promise1 = Promise.resolve('Hello World');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+// Async / Await
+// async function init() {
+//     await createPost({ title: 'Post Three', body: 'This is post three'});
 
-Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values))
+//     getPosts();
+// }
+
+// init();
+
+
+// Async / Await / Fetch
+
+async function fetchUsers() {
+    const res = await fetch
+    ('https://jsonplaceholder.typicode.com/users');
+
+    const data = await res.json();
+
+    console.log(data)
+}
+
+fetchUsers();
+
+// Promise.all
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+
+// Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values))
